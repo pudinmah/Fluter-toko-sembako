4ec724b9ebb5beca0db68c06c4ab47fee826de25
Mahpudin <pudinmah69@gmail.com>
2024-02-14 19:29:34 +0700

    01 item_widget.dart

---
+++ android/app/src/main/kotlin/com/example/flutter_toko/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_toko
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_toko/data.dart';
+import 'package:flutter_toko/item_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final data =
+      Product('Bayam', '2.000', '1ikat', 'assets/img1.png', 'Sayuran sehat');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        title: const Text(
+          'Toko Buah & Sayur',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              Stack(
+                children: [
+                  IconButton(
+                    onPressed: (() {}),
+                    icon: const Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Positioned(
+                      top: 0,
+                      right: 3,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "2",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        ),
+                      ))
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            childAspectRatio: 0.6,
+          ),
+          itemBuilder: (context, index) {
+            return ItemWidget(product: allData[index]);
+          },
+          itemCount: allData.length,
+        ),
+      ),
+    );
+  }
+}
